4d8a260b98372b9152ca7dc84a6ef0fd52413a7d
Erlimar Silva Campos <erlimar@gmail.com>
2014-12-29 19:07:40 -0200

    Step 1
    Hello word com project.json mínimo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System.Console;
+
+namespace E5R.Learning.WelcomeAspNet
+{
+    public class Program
+    {
+        public void Main()
+        {
+            WriteLine("Hello world!");
+        }
+    }
+}
